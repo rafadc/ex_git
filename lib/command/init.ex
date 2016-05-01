418fcbed42d617717d21444c17e7bc04c183a4f8
Rafa de Castro <rafael@micubiculo.com>
2016-05-01 09:30:43 +0200

    Added the description of the repo

--- lib/command/init.ex
+++ lib/command/init.ex
@@ -7,8 +7,14 @@ defmodule ExGit.Command.Init do
     "init - Initialize a Git repository"
   end
 
-  def execute(args) do
+  def execute(_args) do
     Enum.each(@folders, fn(folder) -> File.mkdir folder end)
+    create_description_file
     IO.puts "Initialized empty ExGit repository in #{System.cwd}"
+  end
+
+  defp create_description_file do
+    text = "Unnamed repository; edit this file 'description' to name the repository."
+    File.write(".git/description", text)
   end
 end

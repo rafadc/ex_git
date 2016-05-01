03e71f72814e9b5ed9b14cb9f608cba9f8876b91
Rafa de Castro <rafael@micubiculo.com>
2016-05-01 10:39:38 +0200

    Hash object stores plain objects

--- lib/command/hash_object.ex
+++ lib/command/hash_object.ex
@@ -2,9 +2,21 @@ defmodule ExGit.Command.HashObject do
   @behaviour ExGit.Command
 
   def description do
-    "hash-object - Stores an object"
+    "hash-object <file> - Stores the object in the file given"
   end
 
-  def execute(_args) do
+  def execute(args) do
+    {:ok, content} = File.read(List.first(args))
+    hash = hash(content)
+    folder = String.slice(hash, 0..1)
+    file = String.slice(hash, 2..-1)
+    File.mkdir(".exgit/objects/#{folder}")
+    File.write(".exgit/objects/#{folder}/#{file}", content)
+  end
+
+  defp hash(content) do
+    :crypto.hash(:sha, content)
+    |> Base.encode16
+    |> String.downcase
   end
 end
